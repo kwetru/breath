3df117c0d15488b24a87b727c2674440bdb28299
kwetru <jkzerz@mail.ru>
2021-11-03 22:13:40 +0300

    Design background

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -7,19 +7,18 @@ class Home extends StatefulWidget{
   State<Home> createState() => _HomeState();
 }
 
+
+
 class _HomeState extends State<Home> {
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan[900],
       appBar: AppBar(
         title: Text('Breath'),
         centerTitle: true,
       ),
-        body: SafeArea(
+        body: CustomPaint(
+          painter: BackgroundSignIn(),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -28,7 +27,8 @@ class _HomeState extends State<Home> {
                     Padding(padding: EdgeInsets.only(top: 70),),
                     Text('Выберите режим тренировки', style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black38,
                     ),),
                     Padding(padding: EdgeInsets.only(top: 65),),
                     ElevatedButton.icon(onPressed: () {
@@ -70,5 +70,44 @@ class _HomeState extends State<Home> {
         ),
     );
 
+  }
+}
+
+class BackgroundSignIn extends CustomPainter{
+  @override
+  void paint (Canvas canvas, Size size){
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    //Blue
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh*0.5);
+    blueWave.quadraticBezierTo(sw*0.5, sh*0.45, sw*0.2, 0);
+    blueWave.close();
+    paint.color = Colors.blue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    //Green
+    Path greenWave = Path();
+    greenWave.lineTo(0, sh);
+    greenWave.lineTo(sw, sh);
+    greenWave.cubicTo(sw*0.87, sh*0.45, sw*0.65, sh*0.55, sw*0.36, sh*0.38 );
+    greenWave.cubicTo(sw*0.52, sh*0.52, sw*0.15, sh*0.15, 0, sh*0.4);
+
+    greenWave.close();
+    paint.color = Colors.greenAccent.shade200;
+    canvas.drawPath(greenWave, paint);
+
+  }
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate){
+    return oldDelegate != this;
   }
 }
