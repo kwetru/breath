01cbe515b938717f5b490be26d16760f141d162f
Наумова Татьяна <naumovatd@gmail.com>
2021-11-29 19:49:07 +0300

    Update text_blanks.dart

--- lib/src/utils/text_blanks.dart
+++ lib/src/utils/text_blanks.dart
@@ -2,13 +2,13 @@ List <String> oneMinuteTextList = ['Упражнение нужно делать
   'Закройте большим пальцем правой руки правую ноздрю, и делайте медленный глубокие вдохи и выдохи через левую.',
   'Затем поменяйте руки.', 'Зажмите безымянным пальцем левой руки левую ноздрю, и делайте глубокие вдохи и выдохи.',
   'Сделайте глубокий вдох, а затем резкий выдох одной струей через нос. Ваш нижний пресс должен напрячься, а диафрагма быть расслаблена.',
-  'Сделайте несколько повторений в комфортном темпе.', ];
+  'Сделайте несколько повторений в комфортном темпе.'];
 List <String> threeMinuteTextList = ['Kolya1', 'Kolya2', 'Kolya3', 'Kolya4', 'Kolya5', 'Kolya6',
   'Kolya7', 'Kolya8', 'Kolya9', 'Kolya10', 'Kolya11', 'Kolya12',
-  'Kolya13', 'Kolya14', 'Kolya15', 'Kolya16', 'Kolya17', 'Kolya18',];
+  'Kolya13', 'Kolya14', 'Kolya15', 'Kolya16', 'Kolya17', 'Kolya18'];
 List <String> fiveMinuteTextList = ['Kolya1', 'Kolya2', 'Kolya3', 'Kolya4', 'Kolya5', 'Kolya6',
   'Kolya7', 'Kolya8', 'Kolya9', 'Kolya10', 'Kolya11', 'Kolya12',
   'Kolya13', 'Kolya14', 'Kolya15', 'Kolya16', 'Kolya17', 'Kolya18',
   'Kolya19', 'Kolya20', 'Kolya21', 'Kolya22', 'Kolya23', 'Kolya24',
-  'Kolya25', 'Kolya26', 'Kolya27', 'Kolya28', 'Kolya29', 'Kolya30',];
+  'Kolya25', 'Kolya26', 'Kolya27', 'Kolya28', 'Kolya29', 'Kolya30'];
 
